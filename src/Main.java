ffd5a415d25d04a4cd3f41ab3101ca1c95f8541d
ruslankorotkov <ruslankorotkov.rus@yandex.ru>
2022-11-29 17:02:36 +0300

    сделано дз 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет, Мир");
+        var weightBoxer1 = 78.2;
+        var weightBoxer2= 82.7;
+        var totalWeight = weightBoxer1 + weightBoxer2;
+        System.out.println("общий вес " + totalWeight + " кг ");
+        var weightDifference = weightBoxer1 - weightBoxer2;
+        System.out.println("разница в весе " + weightDifference + " кг ");
+        var weightDifferences = weightBoxer2 -weightBoxer1;
+        System.out.println("вес первого боксёра меньше на  " + weightDifferences + " кг ");
+        var weightDifferencess = weightBoxer2 % weightBoxer1;
+        System.out.println("вес первого боксёра меньше на  " + weightDifferences + " кг при использовании функции остаток от деления ");
+        var workHours = 640;
+        var oneWorker = 8;
+        var workers = workHours / oneWorker;
+        System.out.println(" всего работников в компании " + workers + " человек ");
+        var workerss =  workers + 94;
+        var hourss = workerss * oneWorker;
+        System.out.println(" Если в компании работает " + workerss + " человек,то всего " + hourss + " часов работы может быть поделено между сотрудниками ");
     }
 }
