5e2fd65e00cae5c2bc1ccd17c1928db3008a5230
ruslankorotkov <ruslankorotkov.rus@yandex.ru>
2022-11-29 13:44:50 +0300

    сделано дз

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
-
-        double dog = 8;
+    public static void main(String[] args) { var dog = 8;
         var cat = 3.6;
         var paper = 763789;
-        dog = dog - 1.6;
-        cat = cat - 3.5;
-        paper = paper - 7639;
+        dog = dog +4;
+        cat = cat +4;
+        paper = paper +4;
         System.out.println(cat);
-        System.out.println(dog);
         System.out.println(paper);
+        System.out.println(dog);
     }
 }
