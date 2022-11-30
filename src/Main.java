1cee8e88891ddf259917aa5c68a61d83df00aec2
ruslankorotkov <ruslankorotkov.rus@yandex.ru>
2022-11-30 13:01:20 +0300

    сделано задание переменные часть 2.задача 1

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,17 @@ public class Main {
         char c = 33;
         char d = '!';
         System.out.println(c + " = " + d);
+        int bitBox1 = 5;
+        byte bitBox2 = 6;
+        short bitBox3 = 7;
+        long bitBox4 = 8;
+        float bitBox5 = (float) 0.345;
+        double bitBox6 = 3445.98;
+        System.out.println( " значение переменной bitBox1 c типом int равно " + bitBox1);
+        System.out.println( " значение переменной bitBox2 c типом  byte равно " + bitBox2);
+        System.out.println( " значение переменной bitBox3 c типом  short равно " + bitBox3);
+        System.out.println( " значение переменной bitBox4 c типом  long равно " + bitBox4);
+        System.out.println( " значение переменной bitBox5 c типом  float равно " + bitBox5);
+        System.out.println( " значение переменной bitBox6 c типом  double равно " + bitBox6);
     }
 }
