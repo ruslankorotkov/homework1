33ccd8d8a95e09945ae61e9e3b64cdd93d070730
ruslankorotkov <ruslankorotkov.rus@yandex.ru>
2022-11-30 16:30:25 +0300

    сделано задание переменные часть 2.задание 4.задача 4

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        char c = 33;
-        char d = '!';
-        System.out.println(c + " = " + d);
-        int bitBox1 = 5;
-        byte bitBox2 = 6;
-        short bitBox3 = 7;
-        long bitBox4 = 8;
-        float bitBox5 = (float) 0.345;
-        double bitBox6 = 3445.98;
-        System.out.println( " значение переменной bitBox1 c типом int равно " + bitBox1);
-        System.out.println( " значение переменной bitBox2 c типом  byte равно " + bitBox2);
-        System.out.println( " значение переменной bitBox3 c типом  short равно " + bitBox3);
-        System.out.println( " значение переменной bitBox4 c типом  long равно " + bitBox4);
-        System.out.println( " значение переменной bitBox5 c типом  float равно " + bitBox5);
-        System.out.println( " значение переменной bitBox6 c типом  double равно " + bitBox6);
+        int bottles2Min = 16;
+        int bottlesMinute = bottles2Min / 2;
+        System.out.println( bottlesMinute );
+        int hourBottles = bottlesMinute * 60;
+        int dayBottles = hourBottles * 24;
+        int Minute20 = 20;
+        int bottlesMinute20 = bottlesMinute * Minute20;
+        int day3Bottles = dayBottles * 3;
+        int monthBottles = dayBottles * 31;
+        System.out.println( "за 20 минут машина произвела бутылок " + bottlesMinute20 + " штук ");
+        System.out.println( "за день машина произвнла бутылок " + dayBottles + " штук ");
+        System.out.println("за 3 дня машина произвнла бутылок " + day3Bottles + " штук " );
+        System.out.println("за месяц машина произвнла бутылок " +monthBottles + " штук " );
+
+
     }
 }
