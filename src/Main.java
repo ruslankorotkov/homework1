a579dca65eb00e30409c770355d53d5c33559fec
ruslankorotkov <ruslankorotkov.rus@yandex.ru>
2022-11-30 17:32:12 +0300

    сделано задание переменные часть 2.задание 4.задача 5

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,25 @@ public class Main {
     public static void main(String[] args) {
         int bottles2Min = 16;
         int bottlesMinute = bottles2Min / 2;
-        System.out.println( bottlesMinute );
+        System.out.println(bottlesMinute);
         int hourBottles = bottlesMinute * 60;
         int dayBottles = hourBottles * 24;
         int Minute20 = 20;
         int bottlesMinute20 = bottlesMinute * Minute20;
         int day3Bottles = dayBottles * 3;
         int monthBottles = dayBottles * 31;
-        System.out.println( "за 20 минут машина произвела бутылок " + bottlesMinute20 + " штук ");
-        System.out.println( "за день машина произвнла бутылок " + dayBottles + " штук ");
-        System.out.println("за 3 дня машина произвнла бутылок " + day3Bottles + " штук " );
-        System.out.println("за месяц машина произвнла бутылок " +monthBottles + " штук " );
+        System.out.println("за 20 минут машина произвела бутылок " + bottlesMinute20 + " штук ");
+        System.out.println("за день машина произвнла бутылок " + dayBottles + " штук ");
+        System.out.println("за 3 дня машина произвнла бутылок " + day3Bottles + " штук ");
+        System.out.println("за месяц машина произвнла бутылок " + monthBottles + " штук ");
+        int bottlesPaint = 120;
+        int whitesBox = 2;
+        int brownBox = 4;
+        int classBottles = whitesBox + brownBox;
+        int box = bottlesPaint / classBottles;
+        int whiteBottles = whitesBox * box;
+        int bottlesBrown = brownBox * box;
+        System.out.println(" в школе, где " + box + " классов, нужно " + whiteBottles + " банок белой краски и " + bottlesBrown + " банок коричневой краски");
 
 
     }
